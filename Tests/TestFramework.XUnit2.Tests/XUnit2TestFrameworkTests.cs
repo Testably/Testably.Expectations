2650f23f9d405323f6c1a420c9040ee1ce4dffd6
Valentin Breuß <vbreuss@gmail.com>
2024-02-03 21:58:52 +0100

    Add TestFramework Tests

---
+++ Tests/TestFramework.XUnit2.Tests/XUnit2TestFrameworkTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Testably.Expectations.Tests;
+
+public sealed class XUnit2TestFrameworkTests
+{
+	[Fact]
+	public void WhenUsingXunitAsTestFramework_ShouldThrowXunitException()
+	{
+		Action act = () => Expect.That(true, Is.False);
+
+		var exception = Assert.Throws<XunitException>(act);
+	}
+}
