a1cb5c74b0c86c4b2f2102efe6c0b21e47b61b54
Valentin Breuß <vbreuss@gmail.com>
2024-02-09 23:57:38 +0100

    Add missing XML doc in polyfill

--- Source/Testably.Expectations/Polyfill/StackTraceHiddenAttribute.cs
+++ Source/Testably.Expectations/Polyfill/StackTraceHiddenAttribute.cs
@@ -2,12 +2,14 @@
 // ReSharper disable once CheckNamespace
 namespace System.Diagnostics;
 
+/// <summary>
+///     Types and Methods attributed with StackTraceHidden will be omitted from the stack trace text shown in
+///     StackTrace.ToString() and Exception.StackTrace
+/// </summary>
 [AttributeUsage(AttributeTargets.Class |
                 AttributeTargets.Method |
                 AttributeTargets.Constructor |
                 AttributeTargets.Struct,
 	Inherited = false)]
-public sealed class StackTraceHiddenAttribute : Attribute
-{
-}
+public sealed class StackTraceHiddenAttribute : Attribute;
 #endif
