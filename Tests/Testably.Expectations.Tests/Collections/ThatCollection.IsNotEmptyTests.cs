72412a99efbce4dccb4dcd6c3c0a2a0246e97155
Valentin Breuß <vbreuss@gmail.com>
2024-10-19 20:37:46 +0200

    Add tests

---
+++ Tests/Testably.Expectations.Tests/Collections/ThatCollection.IsNotEmptyTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Testably.Expectations.Tests.Collections;
+
+public sealed partial class ThatCollection
+{
+	public sealed class IsNotEmptyTests
+	{
+		[Fact]
+		public async Task WhenCollectionIsEmpty_ShouldFail()
+		{
+			int[] collection = [];
+
+			async Task Act()
+				=> await Expect.That(collection).IsNotEmpty();
+
+			await Expect.That(Act).Throws<XunitException>()
+				.Which.HasMessage("""
+				                  Expected that collection
+				                  is not empty,
+				                  but it was
+				                  at Expect.That(collection).IsNotEmpty()
+				                  """);
+		}
+
+		[Fact]
+		public async Task WhenCollectionContainsValues_ShouldSucceed()
+		{
+			int[] collection = [1, 1, 2];
+
+			async Task Act()
+				=> await Expect.That(collection).IsNotEmpty();
+
+			await Expect.That(Act).DoesNotThrow();
+		}
+	}
+}
