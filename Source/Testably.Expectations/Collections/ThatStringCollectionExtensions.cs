2d9ee88ad4f803fa8c4fd64c5c63219a026c6c6d
Valentin Breuß <vbreuss@gmail.com>
2024-10-19 17:23:21 +0200

    Add contains for string collections

---
+++ Source/Testably.Expectations/Collections/ThatStringCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Testably.Expectations.Core;
+using Testably.Expectations.Core.Formatting;
+using Testably.Expectations.Core.Helpers;
+
+// ReSharper disable once CheckNamespace
+namespace Testably.Expectations;
+
+/// <summary>
+///     Expectations on <see cref="string" /> collections.
+/// </summary>
+public static class ThatStringCollectionExtensions
+{
+	/// <summary>
+	///     Verifies that the actual collection contains the <paramref name="expected" /> value.
+	/// </summary>
+	public static AssertionResult<IEnumerable<string>, That<IEnumerable<string>>> Contains(this That<IEnumerable<string>> source,
+		string expected,
+		[CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
+		=> new(source.ExpectationBuilder.Add(new ContainsExpectation(expected),
+				b => b.AppendMethod(nameof(Contains), doNotPopulateThisValue)),
+			source);
+
+	private readonly struct ContainsExpectation(string expected) : IExpectation<IEnumerable<string>>
+	{
+		public ExpectationResult IsMetBy(IEnumerable<string> actual)
+		{
+			var list = actual.ToList();
+			if (list.Contains(expected))
+			{
+				return new ExpectationResult.Success<IEnumerable<string>>(list, ToString());
+			}
+
+			return new ExpectationResult.Failure(ToString(), $"found {Formatter.Format(list)}");
+		}
+
+		public override string ToString()
+			=> $"contains {Formatter.Format(expected)}";
+	}
+}
---
+++ Source/Testably.Expectations/Expect.Collections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Testably.Expectations.Core;
+
+namespace Testably.Expectations;
+
+public static partial class Expect
+{
+	/// <summary>
+	///     Start delegate assertions on the current <see cref="string" /> values.
+	/// </summary>
+	public static That<IEnumerable<string>> That(IEnumerable<string> subject,
+		[CallerArgumentExpression("subject")] string doNotPopulateThisValue = "")
+		=> new(new ExpectationBuilder<IEnumerable<string>>(subject, doNotPopulateThisValue));
+
+}
