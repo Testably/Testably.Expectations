d722581657b8ebe4a8ee2bd7af4b86a7995fb95f
Valentin Breuß <vbreuss@gmail.com>
2024-10-20 09:09:15 +0200

    Fix cleanup

--- Source/Testably.Expectations/Core/Formatting/Formatters/TypeFormatter.cs
+++ Source/Testably.Expectations/Core/Formatting/Formatters/TypeFormatter.cs
@@ -9,60 +9,24 @@ internal class TypeFormatter : FormatterBase<Type>
 {
 	private static readonly Dictionary<Type, string> Aliases = new()
 	{
-		{
-			typeof(byte), "byte"
-		},
-		{
-			typeof(sbyte), "sbyte"
-		},
-		{
-			typeof(short), "short"
-		},
-		{
-			typeof(ushort), "ushort"
-		},
-		{
-			typeof(int), "int"
-		},
-		{
-			typeof(uint), "uint"
-		},
-		{
-			typeof(long), "long"
-		},
-		{
-			typeof(ulong), "ulong"
-		},
-		{
-			typeof(float), "float"
-		},
-		{
-			typeof(double), "double"
-		},
-		{
-			typeof(decimal), "decimal"
-		},
-		{
-			typeof(object), "object"
-		},
-		{
-			typeof(bool), "bool"
-		},
-		{
-			typeof(char), "char"
-		},
-		{
-			typeof(string), "string"
-		},
-		{
-			typeof(void), "void"
-		},
-		{
-			typeof(nint), "nint"
-		},
-		{
-			typeof(nuint), "nuint"
-		},
+		{ typeof(byte), "byte" },
+		{ typeof(sbyte), "sbyte" },
+		{ typeof(short), "short" },
+		{ typeof(ushort), "ushort" },
+		{ typeof(int), "int" },
+		{ typeof(uint), "uint" },
+		{ typeof(long), "long" },
+		{ typeof(ulong), "ulong" },
+		{ typeof(float), "float" },
+		{ typeof(double), "double" },
+		{ typeof(decimal), "decimal" },
+		{ typeof(object), "object" },
+		{ typeof(bool), "bool" },
+		{ typeof(char), "char" },
+		{ typeof(string), "string" },
+		{ typeof(void), "void" },
+		{ typeof(nint), "nint" },
+		{ typeof(nuint), "nuint" },
 	};
 
 	/// <inheritdoc />
--- Tests/Testably.Expectations.Tests/Core/Formatting/Formatters/TypeFormatterTests.cs
+++ Tests/Testably.Expectations.Tests/Core/Formatting/Formatters/TypeFormatterTests.cs
@@ -12,105 +12,39 @@ public sealed class TypeFormatterTests
 	public static TheoryData<Type, string> SimpleTypes
 		=> new()
 		{
-			{
-				typeof(int), "int"
-			},
-			{
-				typeof(int?), "int?"
-			},
-			{
-				typeof(uint), "uint"
-			},
-			{
-				typeof(uint?), "uint?"
-			},
-			{
-				typeof(nint), "nint"
-			},
-			{
-				typeof(nint?), "nint?"
-			},
-			{
-				typeof(nuint), "nuint"
-			},
-			{
-				typeof(nuint?), "nuint?"
-			},
-			{
-				typeof(byte), "byte"
-			},
-			{
-				typeof(byte?), "byte?"
-			},
-			{
-				typeof(sbyte), "sbyte"
-			},
-			{
-				typeof(sbyte?), "sbyte?"
-			},
-			{
-				typeof(short), "short"
-			},
-			{
-				typeof(short?), "short?"
-			},
-			{
-				typeof(ushort), "ushort"
-			},
-			{
-				typeof(ushort?), "ushort?"
-			},
-			{
-				typeof(long), "long"
-			},
-			{
-				typeof(long?), "long?"
-			},
-			{
-				typeof(ulong), "ulong"
-			},
-			{
-				typeof(ulong?), "ulong?"
-			},
-			{
-				typeof(float), "float"
-			},
-			{
-				typeof(float?), "float?"
-			},
-			{
-				typeof(double), "double"
-			},
-			{
-				typeof(double?), "double?"
-			},
-			{
-				typeof(decimal), "decimal"
-			},
-			{
-				typeof(decimal?), "decimal?"
-			},
-			{
-				typeof(string), "string"
-			},
-			{
-				typeof(object), "object"
-			},
-			{
-				typeof(bool), "bool"
-			},
-			{
-				typeof(bool?), "bool?"
-			},
-			{
-				typeof(char), "char"
-			},
-			{
-				typeof(char?), "char?"
-			},
-			{
-				typeof(void), "void"
-			},
+			{ typeof(int), "int" },
+			{ typeof(int?), "int?" },
+			{ typeof(uint), "uint" },
+			{ typeof(uint?), "uint?" },
+			{ typeof(nint), "nint" },
+			{ typeof(nint?), "nint?" },
+			{ typeof(nuint), "nuint" },
+			{ typeof(nuint?), "nuint?" },
+			{ typeof(byte), "byte" },
+			{ typeof(byte?), "byte?" },
+			{ typeof(sbyte), "sbyte" },
+			{ typeof(sbyte?), "sbyte?" },
+			{ typeof(short), "short" },
+			{ typeof(short?), "short?" },
+			{ typeof(ushort), "ushort" },
+			{ typeof(ushort?), "ushort?" },
+			{ typeof(long), "long" },
+			{ typeof(long?), "long?" },
+			{ typeof(ulong), "ulong" },
+			{ typeof(ulong?), "ulong?" },
+			{ typeof(float), "float" },
+			{ typeof(float?), "float?" },
+			{ typeof(double), "double" },
+			{ typeof(double?), "double?" },
+			{ typeof(decimal), "decimal" },
+			{ typeof(decimal?), "decimal?" },
+			{ typeof(string), "string" },
+			{ typeof(object), "object" },
+			{ typeof(bool), "bool" },
+			{ typeof(bool?), "bool?" },
+			{ typeof(char), "char" },
+			{ typeof(char?), "char?" },
+			{ typeof(void), "void" },
 		};
 
 	#endregion
