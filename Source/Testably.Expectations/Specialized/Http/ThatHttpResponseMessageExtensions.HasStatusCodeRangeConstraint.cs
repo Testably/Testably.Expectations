c25662f4828f7922fe6bcbdbe4f59011f6222ce9
Valentin Breuß <vbreuss@gmail.com>
2024-10-21 10:37:44 +0200

    feat: add check for HTTP status codes (#27)
    Add the following assertions for `HttpResponseMessage`:
    - `IsSuccessful()`
      Checks for 2xx status codes
    - `IsRedirection()`
      Checks for 3xx status codes
    - `HasClientError()`
      Checks for 4xx status codes
    - `HasServerError()`
      Checks for 5xx status codes
    - `HasError()`
      Checks for 4xx or 5xx status codes
    - `HasStatusCode(HttpStatusCode)`
    - `DoesNotHaveStatusCode(HttpStatusCode)`
    
    Also include the HTTP request information when checking for status
    codes.

---
+++ Source/Testably.Expectations/Specialized/Http/ThatHttpResponseMessageExtensions.HasStatusCodeRangeConstraint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Testably.Expectations.Core.Constraints;
+using Testably.Expectations.Core.Formatting;
+
+// ReSharper disable once CheckNamespace
+namespace Testably.Expectations;
+
+public static partial class ThatHttpResponseMessageExtensions
+{
+	private readonly struct HasStatusCodeRangeConstraint(Func<int, bool> predicate, string expectation)
+		: IAsyncConstraint<HttpResponseMessage>
+	{
+		public async Task<ConstraintResult> IsMetBy(HttpResponseMessage? actual)
+		{
+			if (actual == null)
+			{
+				return new ConstraintResult.Failure<HttpResponseMessage?>(actual, ToString(),
+					"found <null>");
+			}
+
+			if (predicate((int)actual.StatusCode))
+			{
+				return new ConstraintResult.Success<HttpResponseMessage?>(actual, ToString());
+			}
+
+			string formattedResponse = await HttpResponseMessageFormatter.Format(actual, "  ");
+			return new ConstraintResult.Failure<HttpResponseMessage?>(actual, ToString(),
+				$"found {Formatter.Format(actual.StatusCode)}:{Environment.NewLine}{formattedResponse}");
+		}
+
+		public override string ToString()
+			=> expectation;
+	}
+}
---
+++ Source/Testably.Expectations/Specialized/Http/ThatHttpResponseMessageExtensions.HttpResponseMessageFormatter.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Testably.Expectations.Core.Helpers;
+
+// ReSharper disable once CheckNamespace
+namespace Testably.Expectations;
+
+public static partial class ThatHttpResponseMessageExtensions
+{
+	private static class HttpResponseMessageFormatter
+	{
+		public static async Task<string> Format(HttpResponseMessage response, string indentation)
+		{
+			StringBuilder messageBuilder = new StringBuilder();
+
+			messageBuilder.Append(indentation)
+				.Append("HTTP/").Append(response.Version)
+				.Append(" ").Append((int)response.StatusCode).Append(" ").Append(response.StatusCode)
+				.AppendLine();
+
+			AppendHeaders(messageBuilder, response.Headers, indentation);
+			await AppendContent(messageBuilder, response.Content, indentation);
+
+			HttpRequestMessage? request = response.RequestMessage;
+			if (request == null)
+			{
+				messageBuilder.Append(indentation).AppendLine("The originating request was <null>");
+			}
+			else
+			{
+				messageBuilder.Append(indentation).AppendLine("The originating request was:");
+				messageBuilder.Append(indentation).Append(indentation).Append(request.Method.ToString().ToUpper()).Append(" ")
+					.Append(request.RequestUri).Append(" HTTP ").Append(request.Version).AppendLine();
+
+				AppendHeaders(messageBuilder, request.Headers, indentation);
+				if (request.Content != null)
+				{
+					await AppendContent(messageBuilder, request.Content, indentation + indentation);
+				}
+			}
+
+			return messageBuilder.ToString().TrimEnd();
+		}
+
+		private static async Task AppendContent(StringBuilder messageBuilder,
+			HttpContent content,
+			string indentation)
+		{
+			if (content is StringContent)
+			{
+				var stringContent = await content.ReadAsStringAsync();
+				messageBuilder.AppendLine(stringContent.Indent(indentation));
+			}
+			else if (content is FormUrlEncodedContent)
+			{
+				var stringContent = await content.ReadAsStringAsync();
+				messageBuilder.AppendLine(stringContent.Indent(indentation));
+			}
+			else
+			{
+				messageBuilder.Append(indentation).AppendLine("Content is binary");
+			}
+		}
+
+		private static void AppendHeaders(
+			StringBuilder messageBuilder,
+			HttpHeaders headers,
+			string indentation)
+		{
+			foreach (KeyValuePair<string, IEnumerable<string>> header in headers
+				.OrderBy(x => x.Key == "Content-Length"))
+			{
+				foreach (string headerValue in header.Value)
+				{
+					messageBuilder.Append(indentation).Append(indentation)
+						.Append(header.Key).Append(": ").AppendLine(headerValue);
+				}
+			}
+		}
+	}
+}
