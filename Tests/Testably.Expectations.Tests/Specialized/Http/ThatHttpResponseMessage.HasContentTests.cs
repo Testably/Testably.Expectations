db7d08b677bddbddc130b37aa4c59afc7dedfacc
Valentin Breuß <vbreuss@gmail.com>
2024-10-21 07:31:38 +0200

    feat: add support for async constraints (#26)
    Add the interface `IAsyncConstraint<TValue>` which allows evaluating the
    constraint asynchronously.
    Add as an example for the `HttpResponseMessage.HasContent` method.

---
+++ Tests/Testably.Expectations.Tests/Specialized/Http/ThatHttpResponseMessage.HasContentTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Testably.Expectations.Tests.Specialized.Http;
+
+public sealed partial class ThatHttpResponseMessage
+{
+	public sealed class HasContentTests
+	{
+		[Fact]
+		public async Task WhenContentDiffersFromExpected_ShouldFail()
+		{
+			string expected = "other content";
+			HttpResponseMessage sut = Create("some content");
+
+			async Task Act()
+				=> await Expect.That(sut).HasContent(expected);
+
+			await Expect.That(Act).Throws<XunitException>()
+				.Which.HasMessage("""
+				                  Expected that sut
+				                  has a string content equal to "other content",
+				                  but found "some content" which differs at index 0:
+				                     ↓ (actual)
+				                    "some content"
+				                    "other content"
+				                     ↑ (expected)
+				                  at Expect.That(sut).HasContent(expected)
+				                  """);
+		}
+
+		[Fact]
+		public async Task WhenContentEqualsExpected_ShouldSucceed()
+		{
+			string expected = "some content";
+			HttpResponseMessage sut = Create(expected);
+
+			async Task Act()
+				=> await Expect.That(sut).HasContent(expected);
+
+			await Expect.That(Act).DoesNotThrow();
+		}
+	}
+}
---
+++ Tests/Testably.Expectations.Tests/Specialized/Http/ThatHttpResponseMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace Testably.Expectations.Tests.Specialized.Http;
+
+public sealed partial class ThatHttpResponseMessage
+{
+	private static HttpResponseMessage Create(string content,
+		HttpStatusCode statusCode = HttpStatusCode.OK)
+	{
+		HttpResponseMessage httpResponseMessage = new HttpResponseMessage(statusCode);
+		httpResponseMessage.Content = new StringContent(content);
+		return httpResponseMessage;
+	}
+}
