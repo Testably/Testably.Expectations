cca62fd0e4a6b1659301546ce8212ccdf2fc7635
Valentin <valentin.breuss@baur.eu>
2024-10-30 12:32:32 +0100

    Add ApiAcceptance tests

---
+++ Tests/Api/Testably.Expectations.Api.Tests/ApiApprovalTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System.Collections;
+
+namespace Testably.Expectations.Api.Tests;
+
+/// <summary>
+///     Whenever a test fails, this means that the public API surface changed.
+///     If the change was intentional, execute the <see cref="ApiAcceptance.AcceptApiChanges()" /> test to take over the
+///     current public API surface. The changes will become part of the pull request and will be reviewed accordingly.
+/// </summary>
+public sealed class ApiApprovalTests
+{
+	[TestCaseSource(typeof(TargetFrameworksTheoryData))]
+	public void VerifyPublicApiForTestablyExpectations(string framework)
+	{
+		const string assemblyName = "Testably.Expectations";
+
+		string publicApi = Helper.CreatePublicApi(framework, assemblyName);
+		string expectedApi = Helper.GetExpectedApi(framework, assemblyName);
+
+		Assert.That(publicApi, Is.EqualTo(expectedApi));
+	}
+
+	private sealed class TargetFrameworksTheoryData : IEnumerable
+	{
+		#region IEnumerable Members
+
+		public IEnumerator GetEnumerator()
+		{
+			foreach (string targetFramework in Helper.GetTargetFrameworks())
+			{
+				yield return new object[]
+				{
+					targetFramework
+				};
+			}
+		}
+
+		#endregion
+	}
+}
