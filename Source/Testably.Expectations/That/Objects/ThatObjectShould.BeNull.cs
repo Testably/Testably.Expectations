fc83d5a1c6fb7b368e07e7f6f3891a88e4725939
Valentin Breuß <vbreuss@gmail.com>
2024-10-31 18:03:00 +0100

    feat: add `BeNull` for objects (#74)
    Add expectations for `BeNull` / `NotBeNull` for objects.

---
+++ Source/Testably.Expectations/That/Objects/ThatObjectShould.BeNull.cs
@@ -0,0 +1,37 @@
+﻿using Testably.Expectations.Core;
+using Testably.Expectations.Formatting;
+using Testably.Expectations.Results;
+
+// ReSharper disable once CheckNamespace
+namespace Testably.Expectations;
+
+public static partial class ThatObjectShould
+{
+	/// <summary>
+	///     Verifies that the subject is null.
+	/// </summary>
+	public static AndOrExpectationResult<object?, IThat<object?>> BeNull(
+		this IThat<object?> source)
+		=> new(source.ExpectationBuilder
+				.AddConstraint(new GenericConstraint<object?>(
+					null,
+					"be null",
+					(a, _) => a is null,
+					(a, _) => $"found {Formatter.Format(a)}"))
+				.AppendMethodStatement(nameof(BeNull)),
+			source);
+
+	/// <summary>
+	///     Verifies that the subject is not null.
+	/// </summary>
+	public static AndOrExpectationResult<object, IThat<object?>> NotBeNull(
+		this IThat<object?> source)
+		=> new(source.ExpectationBuilder
+				.AddConstraint(new GenericConstraint<object?>(
+					null,
+					"not be null",
+					(a, _) => a is not null,
+					(_, _) => "it was"))
+				.AppendMethodStatement(nameof(NotBeNull)),
+			source);
+}
