449a31b3a2daf26600bd3ea2b5482678acd0eb50
Valentin <valentin.breuss@baur.eu>
2024-11-02 21:22:59 +0100

    Add tests for delegate types

--- Tests/Testably.Expectations.Tests/ThatTests/Delegates/DelegateShould.cs
+++ Tests/Testably.Expectations.Tests/ThatTests/Delegates/DelegateShould.cs
@@ -4,6 +4,149 @@ namespace Testably.Expectations.Tests.ThatTests.Delegates;
 
 public sealed partial class DelegateShould
 {
+	public sealed class Tests
+	{
+		[Fact]
+		public async Task ShouldSupportDelegate_Action()
+		{
+			Action @delegate = () => { };
+
+			async Task Act()
+				=> await That(@delegate).Should().ThrowException();
+
+			await That(Act).Should().ThrowException()
+				.WithMessage("""
+				             Expected @delegate to
+				             throw an Exception,
+				             but it did not
+				             at Expect.That(@delegate).Should().ThrowException()
+				             """);
+		}
+
+		[Fact]
+		public async Task ShouldSupportDelegate_FuncTask()
+		{
+			Func<Task> @delegate = () => Task.CompletedTask;
+
+			async Task Act()
+				=> await That(@delegate).Should().ThrowException();
+
+			await That(Act).Should().ThrowException()
+				.WithMessage("""
+				             Expected @delegate to
+				             throw an Exception,
+				             but it did not
+				             at Expect.That(@delegate).Should().ThrowException()
+				             """);
+		}
+
+		[Fact]
+		public async Task ShouldSupportDelegate_FuncTaskValue()
+		{
+			Func<Task<int>> @delegate = () => Task.FromResult(1);
+
+			async Task Act()
+				=> await That(@delegate).Should().ThrowException();
+
+			await That(Act).Should().ThrowException()
+				.WithMessage("""
+				             Expected @delegate to
+				             throw an Exception,
+				             but it did not
+				             at Expect.That(@delegate).Should().ThrowException()
+				             """);
+		}
+
+		[Fact]
+		public async Task ShouldSupportDelegate_FuncValue()
+		{
+			Func<int> @delegate = () => 1;
+
+			async Task Act()
+				=> await That(@delegate).Should().ThrowException();
+
+			await That(Act).Should().ThrowException()
+				.WithMessage("""
+				             Expected @delegate to
+				             throw an Exception,
+				             but it did not
+				             at Expect.That(@delegate).Should().ThrowException()
+				             """);
+		}
+
+		[Fact]
+		public async Task ShouldSupportDelegate_Task()
+		{
+			Task @delegate = Task.CompletedTask;
+
+			async Task Act()
+				=> await That(@delegate).Should().ThrowException();
+
+			await That(Act).Should().ThrowException()
+				.WithMessage("""
+				             Expected @delegate to
+				             throw an Exception,
+				             but it did not
+				             at Expect.That(@delegate).Should().ThrowException()
+				             """);
+		}
+
+		[Fact]
+		public async Task ShouldSupportDelegate_TaskValue()
+		{
+			Task<int> @delegate = Task.FromResult(1);
+
+			async Task Act()
+				=> await That(@delegate).Should().ThrowException();
+
+			await That(Act).Should().ThrowException()
+				.WithMessage("""
+				             Expected @delegate to
+				             throw an Exception,
+				             but it did not
+				             at Expect.That(@delegate).Should().ThrowException()
+				             """);
+		}
+
+#if NET6_0_OR_GREATER
+		[Fact]
+		public async Task ShouldSupportDelegate_ValueTask()
+		{
+			ValueTask @delegate = ValueTask.CompletedTask;
+
+			async Task Act()
+				=> await That(@delegate).Should().ThrowException();
+
+			await That(Act).Should().ThrowException()
+				.WithMessage("""
+				             Expected @delegate to
+				             throw an Exception,
+				             but it did not
+				             at Expect.That(@delegate).Should().ThrowException()
+				             """);
+		}
+#endif
+
+#if NET6_0_OR_GREATER
+		[Fact]
+		public async Task ShouldSupportDelegate_ValueTaskValue()
+		{
+			ValueTask<int> @delegate = ValueTask.FromResult(1);
+
+			async Task Act()
+				=> await That(@delegate).Should().ThrowException();
+
+			await That(Act).Should().ThrowException()
+				.WithMessage("""
+				             Expected @delegate to
+				             throw an Exception,
+				             but it did not
+				             at Expect.That(@delegate).Should().ThrowException()
+				             """);
+		}
+#endif
+	}
+
 	public class CustomException(
 		[CallerMemberName] string message = "",
 		Exception? innerException = null)
