6dd387362b352bf5f05d40c7144e3652c23908b6
Valentin <valentin.breuss@baur.eu>
2024-11-02 20:56:20 +0100

    Fix incorrect implementation of `BeEmpty` for IAsyncEnumerable

---
+++ Tests/Testably.Expectations.Tests/ThatTests/Collections/AsyncEnumerableShould.AllTests.cs
@@ -0,0 +1,52 @@
+﻿#if NET6_0_OR_GREATER
+using System.Collections.Generic;
+using Testably.Expectations.Tests.TestHelpers;
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace Testably.Expectations.Tests.ThatTests.Collections;
+
+public sealed partial class AsyncEnumerableShould
+{
+	public sealed class AllTests
+	{
+		[Fact]
+		public async Task WhenEnumerableContainsDifferentValues_ShouldFail()
+		{
+			IAsyncEnumerable<int> subject = ToAsyncEnumerable([1, 1, 1, 1, 2, 2, 3]);
+
+			async Task Act()
+				=> await That(subject).Should().All().Be(1);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             have all items equal to 1,
+				             but not all items were equal
+				             at Expect.That(subject).Should().All().Be(1)
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenEnumerableIsEmpty_ShouldSucceed()
+		{
+			IAsyncEnumerable<int> subject = ToAsyncEnumerable([]);
+
+			async Task Act()
+				=> await That(subject).Should().All().Be(0);
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenEnumerableOnlyContainsEqualValues_ShouldSucceed()
+		{
+			IAsyncEnumerable<int> subject = ToAsyncEnumerable([1, 1, 1, 1, 1, 1, 1]);
+
+			async Task Act()
+				=> await That(subject).Should().All().Be(1);
+
+			await That(Act).Should().NotThrow();
+		}
+	}
+}
+#endif
