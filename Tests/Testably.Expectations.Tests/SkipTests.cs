79aacd088b5b181642c25e149482cda0511b4dba
Valentin Breuß <vbreuss@gmail.com>
2024-11-02 07:17:02 +0100

    refactor: add tests for `Skip` and `Fail` (#88)
    #65

---
+++ Tests/Testably.Expectations.Tests/SkipTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Testably.Expectations.Tests;
+
+public class SkipTests
+{
+	[Theory]
+	[AutoData]
+	public async Task ThrownException_ShouldContainReason(string message)
+	{
+		void Act()
+			=> Skip.Test(message);
+
+		await That(Act).Should().ThrowException()
+			.WithMessage($"*{message}*").AsWildcard();
+	}
+
+	[Theory]
+	[InlineAutoData(true)]
+	[InlineAutoData(false)]
+	public async Task Unless_ShouldThrowExceptionWhenTrue(bool condition, string message)
+	{
+		void Act()
+			=> Skip.Unless(condition, message);
+
+		await That(Act).Should().ThrowException().OnlyIf(!condition)
+			.WithMessage($"*{message}*").AsWildcard();
+	}
+
+	[Theory]
+	[InlineAutoData(true)]
+	[InlineAutoData(false)]
+	public async Task When_ShouldThrowExceptionWhenTrue(bool condition, string message)
+	{
+		void Act()
+			=> Skip.When(condition, message);
+
+		await That(Act).Should().ThrowException().OnlyIf(condition)
+			.WithMessage($"*{message}*").AsWildcard();
+	}
+}
