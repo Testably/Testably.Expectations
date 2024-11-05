163d7fc46038fcaaf44067a103813b79a2f935e9
Valentin Breuß <vbreuss@gmail.com>
2024-11-05 17:39:56 +0100

    fix: use correct type for buffer size (#115)
    Fixes #114
    
    Also fix the incorrect XML comment for `NotHaveBufferSize`

--- Source/Testably.Expectations/That/Streams/ThatBufferedStreamShould.HaveBufferSize.cs
+++ Source/Testably.Expectations/That/Streams/ThatBufferedStreamShould.HaveBufferSize.cs
@@ -14,7 +14,7 @@ public static partial class ThatBufferedStreamShould
 	/// </summary>
 	public static AndOrResult<BufferedStream?, IThat<BufferedStream?>> HaveBufferSize(
 		this IThat<BufferedStream?> source,
-		long expected,
+		int expected,
 		[CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
 		=> new(source.ExpectationBuilder
 				.AddConstraint(new ValueConstraint(
@@ -27,16 +27,18 @@ public static partial class ThatBufferedStreamShould
 			source);
 
 	/// <summary>
-	///     Verifies that the subject <see cref="BufferedStream" /> has the <paramref name="expected" /> buffer size.
+	///     Verifies that the subject <see cref="BufferedStream" /> does not have the <paramref name="unexpected" />
+	///     buffer size.
 	/// </summary>
 	public static AndOrResult<BufferedStream?, IThat<BufferedStream?>>
 		NotHaveBufferSize(this IThat<BufferedStream?> source,
-			long expected,
-			[CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
+			int unexpected,
+			[CallerArgumentExpression("unexpected")]
+			string doNotPopulateThisValue = "")
 		=> new(source.ExpectationBuilder
 				.AddConstraint(new ValueConstraint(
-					$"not have buffer size {expected}",
-					actual => actual != null && actual.BufferSize != expected,
+					$"not have buffer size {unexpected}",
+					actual => actual != null && actual.BufferSize != unexpected,
 					actual => actual == null ? "found <null>" : "it had"))
 				.AppendMethodStatement(nameof(NotHaveBufferSize), doNotPopulateThisValue),
 			source);
