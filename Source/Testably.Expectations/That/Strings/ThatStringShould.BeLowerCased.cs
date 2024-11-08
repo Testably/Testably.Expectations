d579d91af4a1b10fb4719c66bb28a031cac62fa3
Valentin Breuß <vbreuss@gmail.com>
2024-11-08 22:15:57 +0100

    feat: add `BeLowerCased` and `BeUpperCased` for strings (#138)
    From #130:
    Add expectations for
    - `BeLowerCased` / `NotBeLowerCased`
    - `BeUpperCased` / `NotBeUpperCased`

---
+++ Source/Testably.Expectations/That/Strings/ThatStringShould.BeLowerCased.cs
@@ -0,0 +1,38 @@
+﻿using Testably.Expectations.Core;
+using Testably.Expectations.Core.Helpers;
+using Testably.Expectations.Formatting;
+using Testably.Expectations.Results;
+
+namespace Testably.Expectations;
+
+public static partial class ThatStringShould
+{
+	/// <summary>
+	///     Verifies that all cased characters in the subject are lower-case.<br />
+	///     That is, that the string could be the result of a call to <see cref="string.ToLowerInvariant()" />,
+	/// </summary>
+	public static AndOrResult<string?, IThat<string?>> BeLowerCased(
+		this IThat<string?> source)
+		=> new(source.ExpectationBuilder
+				.AddConstraint(new GenericConstraint(
+					"",
+					_ => "be lower-cased",
+					(a, _) => a != null && a == a.ToLowerInvariant(),
+					(a, _)
+						=> $"found {Formatter.Format(a.DisplayWhitespace().TruncateWithEllipsis(100))}")),
+			source);
+
+	/// <summary>
+	///     Verifies that of all cased characters in the subject some are upper-case.<br />
+	///     That is, that the string could not be the result of a call to <see cref="string.ToLowerInvariant()" />,
+	/// </summary>
+	public static AndOrResult<string, IThat<string?>> NotBeLowerCased(
+		this IThat<string?> source)
+		=> new(source.ExpectationBuilder
+				.AddConstraint(new GenericConstraint(
+					"",
+					_ => "not be lower-cased",
+					(a, _) => a == null || a != a.ToLowerInvariant(),
+					(a, _) => $"found {Formatter.Format(a.DisplayWhitespace().TruncateWithEllipsis(100))}")),
+			source);
+}
---
+++ Tests/Testably.Expectations.Tests/ThatTests/Strings/StringShould.BeUpperCasedTests.cs
@@ -0,0 +1,258 @@
+﻿namespace Testably.Expectations.Tests.ThatTests.Strings;
+
+public sealed partial class StringShould
+{
+	public class BeUpperCasedTests
+	{
+		[Fact]
+		public async Task WhenActualIsEmpty_ShouldSucceed()
+		{
+			string subject = "";
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsLowerCased_ShouldFail()
+		{
+			string subject = "abc";
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             be upper-cased,
+				             but found "abc".
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsMixedCased_ShouldFail()
+		{
+			string subject = "AbC";
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             be upper-cased,
+				             but found "AbC".
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsNotUpperCased_ShouldLimitDisplayedStringTo100Characters()
+		{
+			string subject = StringWithMoreThan100Characters;
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              be upper-cased,
+				              but found "{StringWith100Characters}…".
+				              """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsNull_ShouldFail()
+		{
+			string? subject = null;
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             be upper-cased,
+				             but found <null>.
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsUpperCased_ShouldSucceed()
+		{
+			string subject = "ABC";
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsUpperCasedOrCaseless_ShouldSucceed()
+		{
+			string subject = "A漢字B";
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsUpperCasedOrSpecialCharacters_ShouldSucceed()
+		{
+			string subject = "A-B-C!";
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsWhitespace_ShouldSucceed()
+		{
+			string subject = " \t\r\n";
+
+			async Task Act()
+				=> await That(subject).Should().BeUpperCased();
+
+			await That(Act).Should().NotThrow();
+		}
+	}
+
+	public sealed class NotBeUpperCasedTests
+	{
+		[Fact]
+		public async Task WhenActualIsEmpty_ShouldFail()
+		{
+			string subject = "";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             not be upper-cased,
+				             but found "".
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsLowerCased_ShouldSucceed()
+		{
+			string subject = "abc";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsMixedCased_ShouldSucceed()
+		{
+			string subject = "AbC";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsNotUpperCased_ShouldLimitDisplayedStringTo100Characters()
+		{
+			string subject = StringWithMoreThan100Characters.ToUpperInvariant();
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              not be upper-cased,
+				              but found "{StringWith100Characters.ToUpperInvariant()}…".
+				              """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsNull_ShouldSucceed()
+		{
+			string? subject = null;
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsUpperCased_ShouldFail()
+		{
+			string subject = "ABC";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             not be upper-cased,
+				             but found "ABC".
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsUpperCasedOrCaseless_ShouldFail()
+		{
+			string subject = "A漢字B";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             not be upper-cased,
+				             but found "A漢字B".
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsUpperCasedOrSpecialCharacters_ShouldFail()
+		{
+			string subject = "A-B-C!";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             not be upper-cased,
+				             but found "A-B-C!".
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsWhitespace_ShouldSucceed()
+		{
+			string subject = " \t ";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeUpperCased();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             not be upper-cased,
+				             but found " \t ".
+				             """);
+		}
+	}
+}
