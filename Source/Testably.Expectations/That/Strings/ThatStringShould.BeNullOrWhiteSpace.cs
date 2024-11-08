4dbda4d96f98e64b9c090550a540588132e7de97
Valentin <valentin.breuss@baur.eu>
2024-11-08 20:42:56 +0100

    Add `BeNullOrWhiteSpace`

---
+++ Source/Testably.Expectations/That/Strings/ThatStringShould.BeNullOrWhiteSpace.cs
@@ -0,0 +1,37 @@
+﻿using Testably.Expectations.Core;
+using Testably.Expectations.Core.Helpers;
+using Testably.Expectations.Formatting;
+using Testably.Expectations.Results;
+
+namespace Testably.Expectations;
+
+public static partial class ThatStringShould
+{
+	/// <summary>
+	///     Verifies that the subject is <see langword="null" />, empty or consists only of white-space characters.
+	/// </summary>
+	public static AndOrResult<string?, IThat<string?>> BeNullOrWhiteSpace(
+		this IThat<string?> source)
+		=> new(source.ExpectationBuilder
+				.AddConstraint(new GenericConstraint(
+					"",
+					_ => "be null or white-space",
+					(a, _) => string.IsNullOrWhiteSpace(a),
+					(a, _)
+						=> $"found {Formatter.Format(a.DisplayWhitespace().TruncateWithEllipsis(100))}")),
+			source);
+
+	/// <summary>
+	///     Verifies that the subject is not <see langword="null" />, empty or consists only of white-space characters.
+	/// </summary>
+	public static AndOrResult<string, IThat<string?>> NotBeNullOrWhiteSpace(
+		this IThat<string?> source)
+		=> new(source.ExpectationBuilder
+				.AddConstraint(new GenericConstraint(
+					"",
+					_ => "not be null or white-space",
+					(a, _) => !string.IsNullOrWhiteSpace(a),
+					(a, _)
+						=> $"found {Formatter.Format(a.DisplayWhitespace().TruncateWithEllipsis(100))}")),
+			source);
+}
---
+++ Tests/Testably.Expectations.Tests/ThatTests/Strings/StringShould.BeNullOrWhiteSpaceTests.cs
@@ -0,0 +1,116 @@
+﻿namespace Testably.Expectations.Tests.ThatTests.Strings;
+
+public sealed partial class StringShould
+{
+	public class BeNullOrWhiteSpaceTests
+	{
+		[Fact]
+		public async Task WhenActualIsEmpty_ShouldSucceed()
+		{
+			string subject = "";
+
+			async Task Act()
+				=> await That(subject).Should().BeNullOrWhiteSpace();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task WhenActualIsNotEmpty_ShouldFail(string? subject)
+		{
+			async Task Act()
+				=> await That(subject).Should().BeNullOrWhiteSpace();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              be null or white-space,
+				              but found "{subject}".
+				              """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsNull_ShouldSucceed()
+		{
+			string? subject = null;
+
+			async Task Act()
+				=> await That(subject).Should().BeNullOrWhiteSpace();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsWhitespace_ShouldFail()
+		{
+			string subject = " \t ";
+
+			async Task Act()
+				=> await That(subject).Should().BeNullOrWhiteSpace();
+
+			await That(Act).Should().NotThrow();
+		}
+	}
+
+	public sealed class NotBeNullOrWhiteSpaceTests
+	{
+		[Fact]
+		public async Task WhenActualIsEmpty_ShouldFail()
+		{
+			string subject = "";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeNullOrWhiteSpace();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             not be null or white-space,
+				             but found "".
+				             """);
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task WhenActualIsNotEmpty_ShouldSucceed(string? subject)
+		{
+			async Task Act()
+				=> await That(subject).Should().NotBeNullOrWhiteSpace();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenActualIsNull_ShouldSucceed()
+		{
+			string? subject = null;
+
+			async Task Act()
+				=> await That(subject).Should().NotBeNullOrWhiteSpace();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             not be null or white-space,
+				             but found <null>.
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenActualIsWhitespace_ShouldSucceed()
+		{
+			string subject = " \t ";
+
+			async Task Act()
+				=> await That(subject).Should().NotBeNullOrWhiteSpace();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected subject to
+				             not be null or white-space,
+				             but found " \t ".
+				             """);
+		}
+	}
+}
