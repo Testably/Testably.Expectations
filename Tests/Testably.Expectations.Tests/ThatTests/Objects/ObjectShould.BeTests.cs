d02efff103c1f1ff7c1dde78eed7b2428e95f73d
Valentin <valentin.breuss@baur.eu>
2024-11-09 19:20:45 +0100

    Add type overload for `object.Be`

--- Tests/Testably.Expectations.Tests/ThatTests/Objects/ObjectShould.BeTests.cs
+++ Tests/Testably.Expectations.Tests/ThatTests/Objects/ObjectShould.BeTests.cs
@@ -6,9 +6,9 @@ public sealed partial class ObjectShould
 	{
 		[Theory]
 		[AutoData]
-		public async Task WhenAwaited_ShouldReturnTypedResult(int value)
+		public async Task ForGeneric_WhenAwaited_ShouldReturnTypedResult(int value)
 		{
-			object? subject = new MyClass { Value = value };
+			object subject = new MyClass { Value = value };
 
 			MyClass result = await That(subject).Should().Be<MyClass>();
 			await That(result.Value).Should().Be(value);
@@ -16,7 +16,7 @@ public sealed partial class ObjectShould
 
 		[Theory]
 		[AutoData]
-		public async Task WhenTypeDoesNotMatch_ShouldFail(int value)
+		public async Task ForGeneric_WhenTypeDoesNotMatch_ShouldFail(int value)
 		{
 			object subject = new MyClass { Value = value };
 
@@ -35,7 +35,7 @@ public sealed partial class ObjectShould
 		}
 
 		[Fact]
-		public async Task WhenTypeIsSubtype_ShouldSucceed()
+		public async Task ForGeneric_WhenTypeIsSubtype_ShouldSucceed()
 		{
 			object subject = new MyClass();
 
@@ -47,7 +47,7 @@ public sealed partial class ObjectShould
 
 		[Theory]
 		[AutoData]
-		public async Task WhenTypeIsSupertype_ShouldFail(int value, string reason)
+		public async Task ForGeneric_WhenTypeIsSupertype_ShouldFail(int value, string reason)
 		{
 			object subject = new MyBaseClass { Value = value };
 
@@ -66,12 +66,85 @@ public sealed partial class ObjectShould
 		}
 
 		[Fact]
-		public async Task WhenTypeMatches_ShouldSucceed()
+		public async Task ForGeneric_WhenTypeMatches_ShouldSucceed()
 		{
 			object subject = new MyClass();
 
 			async Task Act()
 				=> await That(subject).Should().Be<MyClass>();
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task ForType_WhenAwaited_ShouldReturnTypedResult(int value)
+		{
+			object subject = new MyClass { Value = value };
+
+			object? result = await That(subject).Should().Be(typeof(MyClass));
+
+			await That(result).Should().BeSameAs(subject);
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task ForType_WhenTypeDoesNotMatch_ShouldFail(int value)
+		{
+			object subject = new MyClass { Value = value };
+
+			async Task Act()
+				=> await That(subject).Should().Be(typeof(OtherClass))
+					.Because("we want to test the failure");
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($$"""
+				               Expected subject to
+				               be type OtherClass, because we want to test the failure,
+				               but found MyClass {
+				                 Value = {{value}}
+				               }
+				               """);
+		}
+
+		[Fact]
+		public async Task ForType_WhenTypeIsSubtype_ShouldSucceed()
+		{
+			object subject = new MyClass();
+
+			async Task Act()
+				=> await That(subject).Should().Be(typeof(MyBaseClass));
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task ForType_WhenTypeIsSupertype_ShouldFail(int value, string reason)
+		{
+			object subject = new MyBaseClass { Value = value };
+
+			async Task Act()
+				=> await That(subject).Should().Be(typeof(MyClass))
+					.Because(reason);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($$"""
+				               Expected subject to
+				               be type MyClass, because {{reason}},
+				               but found MyBaseClass {
+				                 Value = {{value}}
+				               }
+				               """);
+		}
+
+		[Fact]
+		public async Task ForType_WhenTypeMatches_ShouldSucceed()
+		{
+			object subject = new MyClass();
+
+			async Task Act()
+				=> await That(subject).Should().Be(typeof(MyClass));
 
 			await That(Act).Should().NotThrow();
 		}
