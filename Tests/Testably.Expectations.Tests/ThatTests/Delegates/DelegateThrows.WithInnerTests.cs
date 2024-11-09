29eebc415734c9389e7d555ad9951ad994928720
Valentin <valentin.breuss@baur.eu>
2024-11-09 19:12:05 +0100

    Add `WithInner`

--- Tests/Testably.Expectations.Tests/ThatTests/Delegates/DelegateThrows.WithInnerTests.cs
+++ Tests/Testably.Expectations.Tests/ThatTests/Delegates/DelegateThrows.WithInnerTests.cs
@@ -6,7 +6,8 @@ public sealed partial class DelegateThrows
 	{
 		[Theory]
 		[AutoData]
-		public async Task WhenAwaited_WithExpectations_ShouldReturnThrownException(string message)
+		public async Task ForGeneric_WhenAwaited_WithExpectations_ShouldReturnThrownException(
+			string message)
 		{
 			Exception exception = new OuterException(innerException: new CustomException(message));
 
@@ -18,7 +19,7 @@ public sealed partial class DelegateThrows
 		}
 
 		[Fact]
-		public async Task WhenAwaited_WithoutExpectations_ShouldReturnThrownException()
+		public async Task ForGeneric_WhenAwaited_WithoutExpectations_ShouldReturnThrownException()
 		{
 			Exception exception = new OuterException(innerException: new CustomException());
 
@@ -30,7 +31,7 @@ public sealed partial class DelegateThrows
 
 		[Theory]
 		[AutoData]
-		public async Task WhenInnerExceptionHasSuperType_ShouldFail(string message)
+		public async Task ForGeneric_WhenInnerExceptionHasSuperType_ShouldFail(string message)
 		{
 			Action action = ()
 				=> throw new OuterException(innerException: new CustomException(message));
@@ -49,7 +50,7 @@ public sealed partial class DelegateThrows
 
 		[Theory]
 		[AutoData]
-		public async Task WhenInnerExceptionHasWrongType_ShouldFail(string message)
+		public async Task ForGeneric_WhenInnerExceptionHasWrongType_ShouldFail(string message)
 		{
 			Action action = ()
 				=> throw new OuterException(innerException: new OtherException(message));
@@ -67,7 +68,7 @@ public sealed partial class DelegateThrows
 		}
 
 		[Fact]
-		public async Task WhenInnerExceptionIsPresent_ShouldSucceed()
+		public async Task ForGeneric_WhenInnerExceptionIsPresent_ShouldSucceed()
 		{
 			Action action = () => throw new OuterException(innerException: new CustomException());
 
@@ -78,7 +79,7 @@ public sealed partial class DelegateThrows
 		}
 
 		[Fact]
-		public async Task WhenInnerExceptionIsSubType_ShouldSucceed()
+		public async Task ForGeneric_WhenInnerExceptionIsSubType_ShouldSucceed()
 		{
 			Action action = ()
 				=> throw new OuterException(innerException: new SubCustomException());
@@ -90,12 +91,115 @@ public sealed partial class DelegateThrows
 		}
 
 		[Fact]
-		public async Task WhenNoInnerExceptionIsPresent_ShouldFail()
+		public async Task ForGeneric_WhenNoInnerExceptionIsPresent_ShouldFail()
 		{
 			Action action = () => throw new OuterException();
 
 			async Task Act()
 				=> await That(action).Should().ThrowException().WithInner<CustomException>();
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage("""
+				             Expected action to
+				             throw an Exception with an inner CustomException,
+				             but found <null>
+				             """);
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task ForType_WhenAwaited_WithExpectations_ShouldReturnThrownException(
+			string message)
+		{
+			Exception exception = new OuterException(innerException: new CustomException(message));
+
+			Exception result = await That(() => throw exception)
+				.Should().ThrowException().WithInner(typeof(CustomException),
+					e => e.HaveMessage(message));
+
+			await That(result).Should().BeSameAs(exception);
+		}
+
+		[Fact]
+		public async Task ForType_WhenAwaited_WithoutExpectations_ShouldReturnThrownException()
+		{
+			Exception exception = new OuterException(innerException: new CustomException());
+
+			Exception result = await That(() => throw exception)
+				.Should().ThrowException().WithInner(typeof(CustomException));
+
+			await That(result).Should().BeSameAs(exception);
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task ForType_WhenInnerExceptionHasSuperType_ShouldFail(string message)
+		{
+			Action action = ()
+				=> throw new OuterException(innerException: new CustomException(message));
+
+			async Task Act()
+				=> await That(action).Should().ThrowException()
+					.WithInner(typeof(SubCustomException));
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected action to
+				              throw an Exception with an inner SubCustomException,
+				              but found a CustomException:
+				                {message}
+				              """);
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task ForType_WhenInnerExceptionHasWrongType_ShouldFail(string message)
+		{
+			Action action = ()
+				=> throw new OuterException(innerException: new OtherException(message));
+
+			async Task Act()
+				=> await That(action).Should().ThrowException().WithInner(typeof(CustomException));
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected action to
+				              throw an Exception with an inner CustomException,
+				              but found an OtherException:
+				                {message}
+				              """);
+		}
+
+		[Fact]
+		public async Task ForType_WhenInnerExceptionIsPresent_ShouldSucceed()
+		{
+			Action action = () => throw new OuterException(innerException: new CustomException());
+
+			async Task Act()
+				=> await That(action).Should().ThrowException().WithInner(typeof(CustomException));
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task ForType_WhenInnerExceptionIsSubType_ShouldSucceed()
+		{
+			Action action = ()
+				=> throw new OuterException(innerException: new SubCustomException());
+
+			async Task Act()
+				=> await That(action).Should().ThrowException().WithInner(typeof(CustomException));
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task ForType_WhenNoInnerExceptionIsPresent_ShouldFail()
+		{
+			Action action = () => throw new OuterException();
+
+			async Task Act()
+				=> await That(action).Should().ThrowException().WithInner(typeof(CustomException));
 
 			await That(Act).Should().Throw<XunitException>()
 				.WithMessage("""
