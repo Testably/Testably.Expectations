89565db993551e2d8c05f55d401de1eda30a0c98
Valentin <valentin.breuss@baur.eu>
2024-11-09 19:45:13 +0100

    Add `BeOneOf` for strings

---
+++ Source/Testably.Expectations/That/Strings/ThatStringShould.BeOneOf.cs
@@ -0,0 +1,106 @@
+﻿using Testably.Expectations.Core;
+using Testably.Expectations.Core.Constraints;
+using Testably.Expectations.Formatting;
+using Testably.Expectations.Options;
+using Testably.Expectations.Results;
+
+namespace Testably.Expectations;
+
+public static partial class ThatStringShould
+{
+	/// <summary>
+	///     Verifies that the subject is one of the <paramref name="expected" /> values.
+	/// </summary>
+	public static StringEqualityResult<string?, IThat<string?>> BeOneOf(
+		this IThat<string?> source,
+		params string?[] expected)
+	{
+		StringEqualityOptions options = new();
+		return new StringEqualityResult<string?, IThat<string?>>(
+			source.ExpectationBuilder
+				.AddConstraint(new BeOneOfConstraint(expected, options)),
+			source,
+			options);
+	}
+
+	/// <summary>
+	///     Verifies that the subject is not one of the <paramref name="unexpected" /> values.
+	/// </summary>
+	public static StringEqualityResult<string?, IThat<string?>> NotBeOneOf(
+		this IThat<string?> source,
+		params string?[] unexpected)
+	{
+		StringEqualityOptions options = new();
+		return new StringEqualityResult<string?, IThat<string?>>(
+			source.ExpectationBuilder
+				.AddConstraint(new NotBeOneOfConstraint(unexpected, options)),
+			source,
+			options);
+	}
+
+	private readonly struct BeOneOfConstraint(
+		string?[] expectedValues,
+		StringEqualityOptions options)
+		: IValueConstraint<string?>
+	{
+		/// <inheritdoc />
+		public ConstraintResult IsMetBy(string? actual)
+		{
+			if (actual is null)
+			{
+				return new ConstraintResult.Failure<string?>(null, ToString(),
+					"found <null>");
+			}
+
+			foreach (string? expectedValue in expectedValues)
+			{
+				if (options.Comparer.Equals(actual, expectedValue))
+				{
+					return new ConstraintResult.Success<string?>(actual, ToString());
+				}
+			}
+
+			return new ConstraintResult.Failure<string?>(actual, ToString(),
+				$"found {Formatter.Format(actual)}");
+		}
+
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			return $"be one of {Formatter.Format(expectedValues)}{options}";
+		}
+	}
+
+	private readonly struct NotBeOneOfConstraint(
+		string?[] unexpectedValues,
+		StringEqualityOptions options)
+		: IValueConstraint<string?>
+	{
+		/// <inheritdoc />
+		public ConstraintResult IsMetBy(string? actual)
+		{
+			if (actual is null)
+			{
+				return new ConstraintResult.Failure<string?>(null, ToString(),
+					"found <null>");
+			}
+
+			foreach (string? unexpectedValue in unexpectedValues)
+			{
+				if (options.Comparer.Equals(actual, unexpectedValue))
+				{
+					return new ConstraintResult.Failure<string?>(actual, ToString(),
+						$"found {Formatter.Format(actual)}");
+				}
+			}
+
+			return new ConstraintResult.Success<string?>(actual, ToString());
+		}
+
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			return $"not be one of {Formatter.Format(unexpectedValues)}{options}";
+		}
+	}
+}
---
+++ Tests/Testably.Expectations.Tests/ThatTests/Strings/StringShould.BeOneOfTests.cs
@@ -0,0 +1,95 @@
+﻿namespace Testably.Expectations.Tests.ThatTests.Strings;
+
+public sealed partial class StringShould
+{
+	public class BeOneOfTests
+	{
+		[Theory]
+		[InlineData("foo", "bar", "baz")]
+		public async Task WhenValueIsDifferentToAllExpected_ShouldFail(
+			string? subject, params string?[] expected)
+		{
+			async Task Act()
+				=> await That(subject).Should().BeOneOf(expected);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              be one of {Formatter.Format(expected)},
+				              but found {Formatter.Format(subject)}
+				              """);
+		}
+
+		[Theory]
+		[InlineData("foo", "bar", "foo", "baz")]
+		public async Task WhenValueIsEqualToAnyExpected_ShouldSucceed(
+			string? subject, params string?[] expected)
+		{
+			async Task Act()
+				=> await That(subject).Should().BeOneOf(expected);
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task WhenValueIsNull_ShouldFail(
+			params string?[] expected)
+		{
+			string? subject = null;
+
+			async Task Act()
+				=> await That(subject).Should().BeOneOf(expected);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              be one of {Formatter.Format(expected)},
+				              but found <null>
+				              """);
+		}
+	}
+
+	public class NotBeOneOfTests
+	{
+		[Theory]
+		[AutoData]
+		public async Task WhenUnexpectedIsNull_ShouldSucceed(
+			int subject)
+		{
+			int? unexpected = null;
+
+			async Task Act()
+				=> await That(subject).Should().NotBeOneOf(unexpected);
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Theory]
+		[InlineData("foo", "bar", "baz")]
+		public async Task WhenValueIsDifferentToAllUnexpected_ShouldSucceed(string subject,
+			params string?[] unexpected)
+		{
+			async Task Act()
+				=> await That(subject).Should().NotBeOneOf(unexpected);
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Theory]
+		[InlineData("foo", "bar", "foo", "baz")]
+		public async Task WhenValueIsEqualToAnyUnexpected_ShouldFail(string subject,
+			params string?[] unexpected)
+		{
+			async Task Act()
+				=> await That(subject).Should().NotBeOneOf(unexpected);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              not be one of {Formatter.Format(unexpected)},
+				              but found {Formatter.Format(subject)}
+				              """);
+		}
+	}
+}
