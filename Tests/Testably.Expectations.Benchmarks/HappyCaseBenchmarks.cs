372d9673affeb0e12faca6e1c4b2ebd2a8c2446d
Valentin Breuß <vbreuss@gmail.com>
2024-11-10 11:23:45 +0100

    feat: add initial benchmark project (#149)
    From #147:
    Add initial benchmarks for comparison between `TUnit.Assertions`,
    `FluentAssertions` and this project:
    - Happy Case for string should be equal
    - Happy Case for bool should be true

---
+++ Tests/Testably.Expectations.Benchmarks/HappyCaseBenchmarks.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+using TUnit.Assertions;
+using TUnit.Assertions.Extensions;
+
+namespace Testably.Expectations.Benchmarks;
+
+[MemoryDiagnoser]
+public class HappyCaseBenchmarks
+{
+	private readonly bool _boolSubject = true;
+	private readonly string _stringExpectation = "foo";
+	private readonly string _stringSubject = "foo";
+
+	[Benchmark]
+	public AndConstraint<BooleanAssertions> Bool_FluentAssertions()
+		=> _boolSubject.Should().BeTrue();
+
+	[Benchmark]
+	public async Task<bool> Bool_TestablyExpectations()
+		=> await Expect.That(_boolSubject).Should().BeTrue();
+
+	[Benchmark]
+	public async Task<bool> Bool_TUnit()
+		=> await Assert.That(_boolSubject).IsTrue();
+
+	[Benchmark]
+	public AndConstraint<StringAssertions> String_FluentAssertions()
+		=> _stringSubject.Should().Be(_stringExpectation);
+
+	[Benchmark]
+	public async Task<string?> String_TestablyExpectations()
+		=> await Expect.That(_stringSubject).Should().Be(_stringExpectation);
+
+	[Benchmark]
+	public async Task<string?> String_TUnit()
+		=> await Assert.That(_stringSubject).IsEqualTo(_stringExpectation);
+}
