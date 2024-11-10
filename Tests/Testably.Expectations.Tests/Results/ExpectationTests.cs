0e98585cfcdbd83851b8c2efdc6c57a6e77b5f65
Valentin <valentin.breuss@baur.eu>
2024-11-10 18:42:36 +0100

    Add expectation tests

---
+++ Tests/Testably.Expectations.Tests/Results/ExpectationTests.cs
@@ -0,0 +1,26 @@
+﻿using Testably.Expectations.Results;
+
+namespace Testably.Expectations.Tests.Results;
+
+public class ExpectationTests
+{
+	[Fact]
+	public async Task Equals_ShouldThrowNotSupportedException()
+	{
+		Expectation sut = That(true).Should().BeTrue();
+
+		bool Act() => sut.Equals(That(true).Should().BeTrue());
+
+		await That(Act).Should().Throw<NotSupportedException>();
+	}
+
+	[Fact]
+	public async Task GetHashCode_ShouldThrowNotSupportedException()
+	{
+		Expectation sut = That(true).Should().BeTrue();
+
+		int Act() => sut.GetHashCode();
+
+		await That(Act).Should().Throw<NotSupportedException>();
+	}
+}
