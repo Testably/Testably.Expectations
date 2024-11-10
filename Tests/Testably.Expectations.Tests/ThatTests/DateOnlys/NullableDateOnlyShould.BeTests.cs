1aaca7885876b26244830eab4dbc177c57b309b5
Valentin Breuß <vbreuss@gmail.com>
2024-11-10 17:04:32 +0100

    refactor: separate expectations and tests in Chronology to their respective types (#154)
    Move the expectations and tests under `Chronology` to their respective
    types. This affects:
    - DateTimes
    - DateTimeOffsets
    - DateOnlys
    - TimeOnlys
    - TimeSpans

---
+++ Tests/Testably.Expectations.Tests/ThatTests/DateOnlys/NullableDateOnlyShould.BeTests.cs
@@ -0,0 +1,70 @@
+﻿#if NET6_0_OR_GREATER
+namespace Testably.Expectations.Tests.ThatTests.DateOnlys;
+
+public sealed partial class NullableDateOnlyShould
+{
+	public sealed class BeTests
+	{
+		[Fact]
+		public async Task WhenSubjectIsDifferent_ShouldFail()
+		{
+			DateOnly? subject = CurrentTime();
+			DateOnly? expected = LaterTime();
+
+			async Task Act()
+				=> await That(subject).Should().Be(expected);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              be {Formatter.Format(expected)},
+				              but found {Formatter.Format(subject)}
+				              """);
+		}
+
+		[Fact]
+		public async Task WhenSubjectIsTheSame_ShouldSucceed()
+		{
+			DateOnly? subject = CurrentTime();
+			DateOnly? expected = subject;
+
+			async Task Act()
+				=> await That(subject).Should().Be(expected);
+
+			await That(Act).Should().NotThrow();
+		}
+	}
+
+	public sealed class NotBeTests
+	{
+		[Fact]
+		public async Task WhenSubjectIsDifferent_ShouldSucceed()
+		{
+			DateOnly? subject = CurrentTime();
+			DateOnly? unexpected = LaterTime();
+
+			async Task Act()
+				=> await That(subject).Should().NotBe(unexpected);
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenSubjectIsTheSame_ShouldFail()
+		{
+			DateOnly? subject = CurrentTime();
+			DateOnly? unexpected = subject;
+
+			async Task Act()
+				=> await That(subject).Should().NotBe(unexpected);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              not be {Formatter.Format(unexpected)},
+				              but found {Formatter.Format(subject)}
+				              """);
+		}
+	}
+}
+#endif
---
+++ Tests/Testably.Expectations.Tests/ThatTests/TimeOnlys/NullableTimeOnlyShould.BeTests.cs
@@ -0,0 +1,70 @@
+﻿#if NET6_0_OR_GREATER
+namespace Testably.Expectations.Tests.ThatTests.TimeOnlys;
+
+public sealed partial class NullableTimeOnlyShould
+{
+	public sealed class BeTests
+	{
+		[Fact]
+		public async Task WhenSubjectIsDifferent_ShouldFail()
+		{
+			TimeOnly? subject = CurrentTime();
+			TimeOnly? expected = LaterTime();
+
+			async Task Act()
+				=> await That(subject).Should().Be(expected);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              be {Formatter.Format(expected)},
+				              but found {Formatter.Format(subject)}
+				              """);
+		}
+
+		[Fact]
+		public async Task WhenSubjectIsTheSame_ShouldSucceed()
+		{
+			TimeOnly? subject = CurrentTime();
+			TimeOnly? expected = subject;
+
+			async Task Act()
+				=> await That(subject).Should().Be(expected);
+
+			await That(Act).Should().NotThrow();
+		}
+	}
+
+	public sealed class NotBeTests
+	{
+		[Fact]
+		public async Task WhenSubjectIsDifferent_ShouldSucceed()
+		{
+			TimeOnly? subject = CurrentTime();
+			TimeOnly? unexpected = LaterTime();
+
+			async Task Act()
+				=> await That(subject).Should().NotBe(unexpected);
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Fact]
+		public async Task WhenSubjectIsTheSame_ShouldFail()
+		{
+			TimeOnly? subject = CurrentTime();
+			TimeOnly? unexpected = subject;
+
+			async Task Act()
+				=> await That(subject).Should().NotBe(unexpected);
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected subject to
+				              not be {Formatter.Format(unexpected)},
+				              but found {Formatter.Format(subject)}
+				              """);
+		}
+	}
+}
+#endif
