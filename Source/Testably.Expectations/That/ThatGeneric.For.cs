8add07a506fa03cd80809e76c2bfec61b5d41a98
Valentin <valentin.breuss@baur.eu>
2024-11-14 06:25:36 +0100

    Change the for syntax to work on `IExpectSubject<T>` instead of `IThat<T>`

---
+++ Source/Testably.Expectations/That/ThatGeneric.For.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using Testably.Expectations.Core;
+using Testably.Expectations.Results;
+
+namespace Testably.Expectations;
+
+public static partial class ThatGeneric
+{
+	/// <summary>
+	///     Verifies the <paramref name="expectations" /> on the property selected by the <paramref name="selector" />.
+	/// </summary>
+	public static AndOrResult<T, IExpectSubject<T>> For<T, TProperty>(
+		this IExpectSubject<T> source,
+		Expression<Func<T, TProperty?>> selector,
+		Action<IExpectSubject<TProperty?>> expectations)
+	{
+		IThat<T> should = source.Should(expectationBuilder => expectationBuilder
+			.ForProperty(
+				PropertyAccessor<T, TProperty?>.FromExpression(selector),
+				(property, expectation) => $"for {property}{expectation}")
+			.AddExpectations(e => expectations(new Expect.ThatSubject<TProperty?>(e))));
+		return new AndOrResult<T, IExpectSubject<T>>(should.ExpectationBuilder, source);
+	}
+}
---
+++ Tests/Testably.Expectations.Tests/ThatTests/GenericForTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Testably.Expectations.Tests.ThatTests;
+
+public sealed class GenericForTests
+{
+	[Theory]
+	[AutoData]
+	public async Task WhenConditionIsNotSatisfied_ShouldFail(int value)
+	{
+		int expectedValue = value + 1;
+		MyClass subject = new() { Value1 = value };
+
+		async Task Act()
+			=> await That(subject)
+				.For(o => o.Value1, v => v.Should().Be(expectedValue));
+
+		await That(Act).Should().Throw<XunitException>()
+			.WithMessage($"""
+			              Expected subject to
+			              for .Value1 be {Formatter.Format(expectedValue)},
+			              but found {Formatter.Format(value)}
+			              """);
+	}
+
+	[Theory]
+	[AutoData]
+	public async Task WhenConditionIsSatisfied_ShouldSucceed(int value)
+	{
+		MyClass subject = new() { Value1 = value };
+
+		async Task Act()
+			=> await That(subject).For(o => o.Value1, v => v.Should().Be(value));
+
+		await That(Act).Should().NotThrow();
+	}
+
+	private sealed class MyClass
+	{
+		public int Value1 { get; set; }
+		public int Value2 { get; set; }
+	}
+}
