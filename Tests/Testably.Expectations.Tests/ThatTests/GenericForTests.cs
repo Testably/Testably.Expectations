eb3493d63df5cfe6542e0202b2ccd055e7884e87
Valentin <valentin.breuss@baur.eu>
2024-11-14 09:32:49 +0100

    Add failing tests

--- Tests/Testably.Expectations.Tests/ThatTests/GenericForTests.cs
+++ Tests/Testably.Expectations.Tests/ThatTests/GenericForTests.cs
@@ -3,20 +3,63 @@
 public sealed class GenericForTests
 {
 	[Theory]
+	[InlineData(true, true, true)]
+	[InlineData(true, false, false)]
+	[InlineData(false, true, false)]
+	[InlineData(false, false, false)]
+	public async Task AndCombination_ShouldVerifyAllExpectations(bool a, bool b, bool expectSuccess)
+	{
+		MyCombinationClass subject = new()
+		{
+			A = a,
+			B = b
+		};
+
+		async Task Act()
+			=> await That(subject)
+				.For(o => o.A, v => v.Should().BeTrue()).And
+				.For(o => o.B, v => v.Should().BeTrue());
+
+		await That(Act).Should().Throw<XunitException>();
+	}
+
+	[Theory]
+	[InlineData(true, true, true)]
+	[InlineData(true, false, true)]
+	[InlineData(false, true, true)]
+	[InlineData(false, false, false)]
+	public async Task OrCombination_ShouldVerifyAnyExpectations(bool a, bool b, bool expectSuccess)
+	{
+		MyCombinationClass subject = new()
+		{
+			A = a,
+			B = b
+		};
+
+		async Task Act()
+			=> await That(subject)
+				.For(o => o.A, v => v.Should().BeTrue()).Or
+				.For(o => o.B, v => v.Should().BeTrue());
+
+		await That(Act).Should().Throw<XunitException>().OnlyIf(!expectSuccess)
+			.WithMessage("foo");
+	}
+
+	[Theory]
 	[AutoData]
 	public async Task WhenConditionIsNotSatisfied_ShouldFail(int value)
 	{
 		int expectedValue = value + 1;
-		MyClass subject = new() { Value1 = value };
+		MyClass subject = new() { Value = value };
 
 		async Task Act()
 			=> await That(subject)
-				.For(o => o.Value1, v => v.Should().Be(expectedValue));
+				.For(o => o.Value, v => v.Should().Be(expectedValue));
 
 		await That(Act).Should().Throw<XunitException>()
 			.WithMessage($"""
 			              Expected subject to
-			              for .Value1 be {Formatter.Format(expectedValue)},
+			              for .Value be {Formatter.Format(expectedValue)},
 			              but found {Formatter.Format(value)}
 			              """);
 	}
@@ -25,17 +68,22 @@ public sealed class GenericForTests
 	[AutoData]
 	public async Task WhenConditionIsSatisfied_ShouldSucceed(int value)
 	{
-		MyClass subject = new() { Value1 = value };
+		MyClass subject = new() { Value = value };
 
 		async Task Act()
-			=> await That(subject).For(o => o.Value1, v => v.Should().Be(value));
+			=> await That(subject).For(o => o.Value, v => v.Should().Be(value));
 
 		await That(Act).Should().NotThrow();
 	}
 
 	private sealed class MyClass
 	{
-		public int Value1 { get; set; }
-		public int Value2 { get; set; }
+		public int Value { get; set; }
+	}
+
+	private sealed class MyCombinationClass
+	{
+		public bool A { get; set; }
+		public bool B { get; set; }
 	}
 }
