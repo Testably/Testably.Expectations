710f69d4f331ab9458901bc72907ff92cf0fcfbc
Valentin <valentin.breuss@baur.eu>
2024-11-17 13:59:59 +0100

    Add StringMatcherTests.cs

--- Tests/Testably.Expectations.Tests/ThatTests/Strings/StringShould.cs
+++ Tests/Testably.Expectations.Tests/ThatTests/Strings/StringShould.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace Testably.Expectations.Tests.ThatTests.Strings;
+﻿namespace Testably.Expectations.Tests.ThatTests.Strings;
 
 public sealed partial class StringShould
 {
@@ -9,35 +7,4 @@ public sealed partial class StringShould
 
 	private static readonly string StringWithMoreThan100Characters =
 		"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore";
-
-	/// <summary>
-	///     A test <see cref="IEqualityComparer{T}" /> for <see langword="string" />s that
-	///     ignores case differences only in vocals.
-	/// </summary>
-	public sealed class IgnoreCaseForVocalsComparer : IEqualityComparer<string>
-	{
-		#region IEqualityComparer<string> Members
-
-		public bool Equals(string? x, string? y)
-		{
-			string? adjustedX = LowercaseVocals(x);
-			string? adjustedY = LowercaseVocals(y);
-
-			return adjustedX?.Equals(adjustedY, StringComparison.Ordinal) == true;
-		}
-
-		public int GetHashCode(string obj)
-		{
-			return obj.GetHashCode();
-		}
-
-		#endregion
-
-		private static string? LowercaseVocals(string? input)
-			=> input?.Replace('A', 'a')
-				.Replace('E', 'e')
-				.Replace('I', 'i')
-				.Replace('O', 'o')
-				.Replace('U', 'u');
-	}
 }
