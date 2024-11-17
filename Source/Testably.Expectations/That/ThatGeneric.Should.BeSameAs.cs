b4f7a87431bf387cdc6c42e5f3fe0c158c51340d
Valentin <valentin.breuss@baur.eu>
2024-11-17 05:31:28 +0100

    Update Generic

--- Source/Testably.Expectations/That/ThatGeneric.Should.BeSameAs.cs
+++ Source/Testably.Expectations/That/ThatGeneric.Should.BeSameAs.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 using Testably.Expectations.Core;
+using Testably.Expectations.Core.Constraints;
 using Testably.Expectations.Formatting;
 using Testably.Expectations.Results;
 
@@ -14,11 +15,8 @@ public static partial class ThatGeneric
 		object? expected,
 		[CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
 		=> new(source.ExpectationBuilder
-				.AddConstraint(new ConditionConstraint<object?>(
-					expected,
-					$"refer to {doNotPopulateThisValue} {Formatter.Format(expected, FormattingOptions.MultipleLines)}",
-					(a, e) => ReferenceEquals(e, a),
-					(a, _) => $"found {Formatter.Format(a, FormattingOptions.MultipleLines)}")),
+				.AddConstraint(it =>
+					new BeSameAsConstraint<T>(it, expected, doNotPopulateThisValue)),
 			source);
 
 	/// <summary>
@@ -29,10 +27,49 @@ public static partial class ThatGeneric
 		[CallerArgumentExpression("unexpected")]
 		string doNotPopulateThisValue = "")
 		=> new(source.ExpectationBuilder
-				.AddConstraint(new ConditionConstraint<object?>(
-					unexpected,
-					$"not refer to {doNotPopulateThisValue} {Formatter.Format(unexpected, FormattingOptions.MultipleLines)}",
-					(a, e) => !ReferenceEquals(e, a),
-					(_, _) => "it did")),
+				.AddConstraint(it =>
+					new NotBeSameAsConstraint<T>(it, unexpected, doNotPopulateThisValue)),
 			source);
+
+	private readonly struct BeSameAsConstraint<T>(
+		string it,
+		object? expected,
+		string expectedExpression)
+		: IValueConstraint<T>
+	{
+		public ConstraintResult IsMetBy(T actual)
+		{
+			if (ReferenceEquals(actual, expected))
+			{
+				return new ConstraintResult.Success<T>(actual, ToString());
+			}
+
+			return new ConstraintResult.Failure(ToString(),
+				$"{it} was {Formatter.Format(actual, FormattingOptions.MultipleLines)}");
+		}
+
+		public override string ToString()
+			=> $"refer to {expectedExpression} {Formatter.Format(expected, FormattingOptions.MultipleLines)}";
+	}
+
+	private readonly struct NotBeSameAsConstraint<T>(
+		string it,
+		object? unexpected,
+		string expectedExpression)
+		: IValueConstraint<T>
+	{
+		public ConstraintResult IsMetBy(T actual)
+		{
+			if (!ReferenceEquals(actual, unexpected))
+			{
+				return new ConstraintResult.Success<T>(actual, ToString());
+			}
+
+			return new ConstraintResult.Failure(ToString(),
+				$"{it} did");
+		}
+
+		public override string ToString()
+			=> $"not refer to {expectedExpression} {Formatter.Format(unexpected, FormattingOptions.MultipleLines)}";
+	}
 }
